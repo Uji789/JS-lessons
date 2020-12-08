e69edcf6af47b5cbedd8774d6db2aa5c7996533b
Evgenia <zebric7@gmail.com>
2020-12-08 10:56:18 +0200

    new version

--- Lesson_js_3_Evgenia/border-radius/js/common.js
+++ Lesson_js_3_Evgenia/border-radius/js/common.js
@@ -1,17 +1,43 @@
-function fun1() {
-    let rngtl=document.getElementById('rangetl').value;
-    let ntl=document.getElementById('numbertl');
-    let rngtr=document.getElementById('rangetr').value;
-    let ntr=document.getElementById('numbertr');
-    let rngbr=document.getElementById('rangebr').value;
-    let nbr=document.getElementById('numberbr');
-    let rngbl=document.getElementById('rangebl').value;
-    let nbl=document.getElementById('numberbl');
-    let block=document.getElementById('block');
-    ntl.value=rngtl;
-    ntr.value=rngtr;
-    nbr.value=rngbr;
-    nbl.value=rngbl;
-    block.style.borderRadius=rngtl+'px '+rngtr+'px '+rngbr+'px '+rngbl+'px ';
-};
-
+document.addEventListener('DOMContentLoaded', function(){
+    const rangetl = document.querySelector('.rangetl');
+    const num = document.querySelector('.num');
+    const block = document.querySelector('.block');
+    const tl = document.querySelector('.tl');
+    const tr = document.querySelector('.tr');
+    const bl = document.querySelector('.bl');
+    const br = document.querySelector('.br');
+    const rangetr = document.querySelector('.rangetr');
+    const rangebl = document.querySelector('.rangebl');
+    const rangebr = document.querySelector('.rangebr');
+    
+       
+    rangetl.addEventListener('input', function(){
+        num.innerText = this.value;
+        //console.log(this.value);
+        block.style.borderTopLeftRadius = this.value + 'px';
+        console.log(block);
+        tl.innerHTML = "Border - top - left - radius: " + this.value + 'px;';
+    });
+    rangetr.addEventListener('input', function(){
+        num.innerText = this.value;
+        //console.log(this.value);
+        block.style.borderTopRightRadius = this.value + 'px';
+        console.log(block);
+        tr.innerHTML = "Border - top - right - radius: " + this.value + 'px;';
+    })
+    rangebl.addEventListener('input', function(){
+        num.innerText = this.value;
+        //console.log(this.value);
+        block.style.borderBottomLeftRadius = this.value + 'px';
+        console.log(block);
+        bl.innerHTML = "Border - bottom - left - radius: " + this.value + 'px;';
+    })
+    rangebr.addEventListener('input', function(){
+        num.innerText = this.value;
+        //console.log(this.value);
+        block.style.borderBottomRightRadius = this.value + 'px';
+        console.log(block);
+        br.innerHTML = "Border - bottom - right - radius: " + this.value + 'px;';
+    })
+    
+});
